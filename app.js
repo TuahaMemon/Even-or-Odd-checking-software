c90334bc8bd04d62b3827d3cf9bacf5996d0add1
Tuaha memon <tuaha.me@gmail.com>
2022-07-14 03:03:26 -0700

    fully working

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 function calculate() {
     let number = document.querySelector("#number").value;
 
-    let result = (number) / 2;
-    document.querySelector("#result").innerHTML = `Your given number is ${result}`;
+    let result = (number) % 2;
+    document.querySelector("#result").innerHTML = `Your answer is ${result}`;
 }
+
+
+
+// if(number % 2 == 0){
+    
+//     document.querySelector("#result").innerHTML = `Your given number is Even ${result}`;
+// }
+// else{
+    
+//     document.querySelector("#result").innerHTML = `Your given number is Odd ${result}`;
+// }
+
+
+
+
+// program to check if the number is even or odd
+// take input from the user
+// const number = prompt("Enter a number: ");
+
+// //check if the number is even
+// if(number / 2 == 0) {
+//     console.log("The number is even.");
+// }
+
+// // if the number is odd
+// else {
+//     console.log("The number is odd.");
+// }
